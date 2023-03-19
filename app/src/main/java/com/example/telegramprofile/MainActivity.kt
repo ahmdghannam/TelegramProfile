0e2dfb0b26601752b45e0e54300f364b338f4f7f
Ahmed Ghannam <iia7mt@gmail.com>
2023-03-19 12:49:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/telegramprofile/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.telegramprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+
+    }
+}
